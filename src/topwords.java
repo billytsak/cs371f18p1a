02b143a0dafac35e1eb46eea967ff828f123440b
wtsakalios <wtsakalios@luc.edu>
2018-09-13 23:19:59 -0500

    final incomplete product

--- src/topwords.java
+++ src/topwords.java
@@ -1,17 +1,17 @@
 //prints an updated word cloud for each sufficiently long word read from the standard input (continuous stream of words)
 
+import java.io.IOException;
+
 public class topwords {
 
-    public static void main(String[] args) { //will accept three positive command-line arguments/integers: howmany, minlength, lastnwords
+    public static void main(String[] args) throws IOException { //will accept three positive command-line arguments/integers: howmany, minlength, lastnwords
         //-----------------------------------------------------------------------------------------------   args[0]   args[1]    args[2]
 
         //calls to the three methods below, passing the args parameters from the user
         int howmany = Integer.parseInt(args[0]);
         int minlength = Integer.parseInt(args[1]);
-//        wordcount.iterator(minlength);
+        wordcount.iterator(minlength);
         int lastnwords = Integer.parseInt(args[2]);
     }
-        //Reader reader = new FileReader(filename); //for a file
-        //Reader reader = new InputStreamReader(socket.getInputStream()); //for an application
 
 }
--- src/wordcount.java
+++ src/wordcount.java
@@ -5,10 +5,11 @@ import java.util.HashMap;
 
 public class wordcount {
 
-    public void iterator(int minlength) throws IOException {
+    public static void iterator(int minlength) throws IOException {
 
 //      reading the words
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));// Reader reader = new FileReader(filename); for a file
+//                                                                                      Reader reader = new InputStreamReader(socket.getInputStream()); for an application
         String line = reader.readLine();
         String[] splitter = line.toLowerCase().split("\\W+");
 
@@ -18,15 +19,18 @@ public class wordcount {
         while (line != null) {
             for (String words : splitter) {
                 if (wordBank.containsKey(words)) { //&& words.length() > minlength
-                    wordBank.put(words, wordBank.get(words) + 1);
+                    wordBank.put(words, wordBank.get(words) + 1); //increase occurrence value of the word if its present in wordbank
                 }
                 else {
-                    wordBank.put(words, 1);
+                    wordBank.put(words, 1); //store word in wordbank and increase occurrence by 1
                 }
-                System.out.println(wordBank);
+                System.out.println(wordBank); //print the updated wordbank after every word is handled
             }
-
-            line = reader.readLine();
+            line = reader.readLine(); //move on to next line of text
         }
+    }
+
+    public void sortWords(){ //will handle sorting the wordbank, possibly with treemap
+
     }
 }
